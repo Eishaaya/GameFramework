c1b8878b89f7c92c4de69ddb5f2daeea5758fd32
Avi ISHAAYA <meishaaya@yahoo.com>
2022-01-20 15:45:49 -0800

    Action button ctors done

---
+++ TestLibrary/ActionButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseGameLibrary
+{
+    class ActionButton
+    {
+        public enum ClickType
+        {
+            Left,
+            Right,
+            Middle
+        }
+
+        Dictionary<ClickType, IParamAction> clickActions;
+        Button button;
+
+        public ActionButton(Button button, IParamAction leftAction = null, IParamAction rightAction = null, IParamAction middleAction = null)
+            : this(button, new Dictionary<ClickType, IParamAction>())
+        {
+            clickActions.Add(ClickType.Left, leftAction);
+            clickActions.Add(ClickType.Right, rightAction);
+            clickActions.Add(ClickType.Middle, middleAction);
+        }
+
+        public ActionButton(Button button, Dictionary<ClickType, IParamAction> clickActions)
+        {
+            this.button = button;
+            this.clickActions = clickActions;
+        }
+    }
+}
--- TestLibrary/ButtonManager.cs
+++ TestLibrary/ButtonManager.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BaseGameLibrary
 {
-    public class ButtonManager
+    class ButtonManager
     {
-        List<IParamFunc> 
+        List<IParamAction> actions;
+        List<Button> buttons;
+        Screen parent;
+
+        public ButtonManager(Screen parent, List<IParamAction> actions, List<Button> buttons)
+        {
+            this.parent = parent;
+            this.actions = actions;
+            this.buttons = buttons;
+        }
+
+        public void Update(Vector2 mousePos, bool leftDown, bool rightDown)
+        {
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                var currentButton = buttons[i];
+
+                if ()
+            }
+        }
+
+        public void Draw()
+        {
+
+        }
     }
 }
